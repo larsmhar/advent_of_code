370b075e13947e685c7f7be11b1b76612f2508ed
larsmhar <lars.martin.hardeland@gmail.com>
2018-12-04 01:53:37 +0100

    Added the correct answer to the sulution

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,3 +1,3 @@
 from functools import reduce
 
-print(reduce(lambda x, prev: int(prev) + int(x), open("data.txt", "r")))
+print(reduce(lambda x, prev: int(prev) + int(x), open("data.txt", "r"))) # 500
