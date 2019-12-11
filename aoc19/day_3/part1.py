72e5f1c03871907a8cdd8ea4685412fd2e0b94e4
larsmhar <lars.martin.hardeland@gmail.com>
2019-12-11 17:56:35 +0100

    Started cleaning up day 3
    Doesn't feel good to work with or to look at

--- aoc19/day_3/part1.py
+++ aoc19/day_3/part1.py
@@ -15,9 +15,15 @@ conversions = {
 
 def get_coords(data, ends = None):
     coords = [set(), set()]
-    minimums = []
+    minimums = [[],[]]
+    return_minimums = True if ends else False
+    if ends:
+        ends_copy = ends
     for i, line_data in enumerate(data):
         x, y = 0, 0
+        print(i)
+        if return_minimums:
+            ends = ends_copy
         for datum in line_data: 
             move = list(int(datum[1:]) * x for x in conversions[datum[0]])
             # Original (smart?) idea
@@ -25,37 +31,33 @@ def get_coords(data, ends = None):
             for step in range(max([abs(x) for x in move])):
                 if ends and ends & coords[i]:
                     intersect = ends.intersection(coords[i])
-                    #print("Intersect", intersect)
                     #minimums.append(ends.intersection(coords[i]))
-                    minimums.append(len(coords[i]))
+                    minimums[i].append(len(coords[i]))
                     #print(ends.intersection(coords[i]))
-                    print(len(coords[i]))
+                    #print(len(coords[i]))
                     ends = ends - coords[i]
                 x, y = (tuple(list(map(lambda x, y: x + y, conversions[datum[0]], [x, y]))))
                 coords[i].add(tuple(list([x, y])))
-    return minimums if ends else coords[0] & coords[1]
+    if return_minimums:
+        return_vals = [0 for i in range(len(minimums[0]))]
+        for xs in minimums:
+            for i, x in enumerate(xs):
+                return_vals[i] += x
+    return return_vals if return_minimums else coords[0] & coords[1]
 
-
-def manhattan_distance(data):
-    return abs(data[0]) + abs(data[1])
 
 clashes = get_coords(data)
-distances = []
-print(clashes)
+#distances = []
+#for coords in clashes:
+    #distances.append((manhattan_distance(coords), coords))
 
-for coords in clashes:
-    distances.append((manhattan_distance(coords), coords))
-
-minimum =(reduce(lambda minimum, x: x if (x[0] < minimum[0]) else minimum, distances))
-print("Minimum", minimum[0]) # 245
+#minimum =(reduce(lambda minimum, x: x if (x[0] < minimum[0]) else minimum, distances))
+#print("Minimum", minimum[0]) # 245
+print("part 1: ", min(abs(x) + abs(y) for (x, y) in clashes))
 minimums = get_coords(data, clashes)
-#minimums = ([len(get_coords(data, x[1])[0]) + len(get_coords(data, x[1])[1]) for x in distances])
 
 #print(minimums)
-#print(min(minimums))
-#print("MIN", ([len(get_coords(data, x[1])[0]) + len(get_coords(data, x[1])[1]) for x in distances]))
+#print(minimums.sort())
+#print(minimums)
+        
 
-
-print(minimums)
-print(minimums.sort())
-print(minimums)
