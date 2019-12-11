42d79b7bb9e827a2bbb23b45143e34e7ad7ec3cf
larsmhar <lars.martin.hardeland@gmail.com>
2019-12-11 17:31:51 +0100

    Revert "Almost (hopefully?) finshed day 2 part 2"
    This reverts commit 9e280454a4c3c9a8ca5cc653cbf03b1e0bfbc2bd.

--- aoc19/day_3/part1.py
+++ aoc19/day_3/part1.py
@@ -1,10 +1,5 @@
-from functools import reduce
-
-
 data = [[x.strip() for x in xs] for xs in list(map(lambda x: x.split(","), open("data.txt", "r")))]
-#data = [[x.strip() for x in xs] for xs in list(map(lambda x: x.split(","), open("testdata1.txt", "r")))]
-#data = [[x.strip() for x in xs] for xs in list(map(lambda x: x.split(","), open("testdata2.txt", "r")))]
-
+print(data)
 
 conversions = {
     "R": [1, 0],
@@ -13,49 +8,43 @@ conversions = {
     "D": [0, -1],
 }
 
-def get_coords(data, ends = None):
+def get_coords(data):
     coords = [set(), set()]
-    minimums = []
     for i, line_data in enumerate(data):
         x, y = 0, 0
         for datum in line_data: 
             move = list(int(datum[1:]) * x for x in conversions[datum[0]])
+            #print(datum, i, move)
+            #print(x, y)
+            #print("this thing", (list(map(lambda x, y: x + y, conversions[datum[0]], [x, y]))))
             # Original (smart?) idea
             #print(list(map(lambda x, y: x + y, move, [-10, -10])))
             for step in range(max([abs(x) for x in move])):
-                if ends and ends & coords[i]:
-                    intersect = ends.intersection(coords[i])
-                    #print("Intersect", intersect)
-                    #minimums.append(ends.intersection(coords[i]))
-                    minimums.append(len(coords[i]))
-                    #print(ends.intersection(coords[i]))
-                    print(len(coords[i]))
-                    ends = ends - coords[i]
+                #print(x, y, conversions[datum[0]])
                 x, y = (tuple(list(map(lambda x, y: x + y, conversions[datum[0]], [x, y]))))
+                #print(x, y, conversions[datum[0]])
                 coords[i].add(tuple(list([x, y])))
-    return minimums if ends else coords[0] & coords[1]
+            """
+            print(coords)
+            b = set()
+            b.add(tuple([1003, 0]))
+            print(coords & b)
+            """
+    return coords[0] & coords[1]
+        #print(list(map(lambda x, y: x + y, move, [-10, -10])))
+        #for _ in range(int(datum[1:])):
 
 
 def manhattan_distance(data):
     return abs(data[0]) + abs(data[1])
 
 clashes = get_coords(data)
-distances = []
 print(clashes)
+print("here")
+distances = []
 
 for coords in clashes:
-    distances.append((manhattan_distance(coords), coords))
+    print(coords)
+    distances.append(manhattan_distance(coords))
 
-minimum =(reduce(lambda minimum, x: x if (x[0] < minimum[0]) else minimum, distances))
-print("Minimum", minimum[0]) # 245
-minimums = get_coords(data, clashes)
-#minimums = ([len(get_coords(data, x[1])[0]) + len(get_coords(data, x[1])[1]) for x in distances])
-
-#print(minimums)
-#print(min(minimums))
-#print("MIN", ([len(get_coords(data, x[1])[0]) + len(get_coords(data, x[1])[1]) for x in distances]))
-
-
-print(minimums)
-print(minimums.sort())
-print(minimums)
+print(min(distances)) # 1024
